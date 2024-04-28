52a89fa0dd8959a63572ce610bff9a3cdb8dd7b2
MilaBozhevskaya <mi.bozhev@gmail.com>
2024-04-28 16:17:01 +0300

    refactor(shared/api/services, shared/api/constants): move base url to constants file
    https://github.com/milabozhevskaya/fronttech/pull/1#discussion_r1578315688

--- src/shared/api/services.ts
+++ src/shared/api/services.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 import type { Repo, ReposQuery, User, UserQuery } from "../types";
+import { BASE_URL } from "../constants";
 
 const services = {
   getUser: async ({ user }: UserQuery) =>
-    axios.get<User>(`https://api.github.com/users/${user}`),
+    axios.get<User>(`${BASE_URL}/users/${user}`),
 
   getRepos: async ({ user, perPage, page }: ReposQuery) =>
     axios.get<Array<Repo>>(
-      `https://api.github.com/users/${user}/repos?per_page=${perPage}&page=${page}&type=public`
+      `${BASE_URL}/users/${user}/repos?per_page=${perPage}&page=${page}&type=public`
     ),
 };
 
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,9 +1,11 @@
+const BASE_URL = "https://api.github.com";
 const QUERY_KEY_USER = "user";
 const QUERY_KEY_REPOS = "repos";
 const REPOS_PER_PAGE = 4;
 const BUTTONS_PER_PAGINATION = 7;
 
 export {
+  BASE_URL,
   QUERY_KEY_USER,
   QUERY_KEY_REPOS,
   REPOS_PER_PAGE,
